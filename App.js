0ca95bf14efad562fc16013a8c6f7f2bbf4db56f
Mohammad Aminul Islam <78461699+Aminul059@users.noreply.github.com>
2021-09-27 18:27:11 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header/Header';
+import WritterContainer from './component/WritterContainer/WritterContainer';
+
+
+function App() {
+  return (
+    <div>
+     <Header></Header>
+     <WritterContainer></WritterContainer>
+     
+    </div>
+  );
+}
+
+export default App;
